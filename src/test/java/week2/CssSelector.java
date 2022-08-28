1bb0c4faa00c398b03d19ff910349cbcd6338de2
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-08-28 18:10:00 +0300

    automation week 2

---
+++ src/test/java/week2/CssSelector.java
@@ -0,0 +1,23 @@
+package week2;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class CssSelector {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com/");
+
+
+        // using css locator
+        WebElement title = driver.findElement(By.cssSelector("div[id=\"flash-messages\"]"));
+        System.out.println(title.getTagName());
+
+
+    }
+}
