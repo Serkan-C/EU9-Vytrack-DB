4e46b4d367830dda3a819181e910bfa127db9cbc
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-09-22 21:25:39 +0300

    week 2 review codes

---
+++ src/test/java/avengerHours/week2/Methods.java
@@ -0,0 +1,40 @@
+package avengerHours.week2;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import java.util.*;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practice.cydeo.com/");
+
+//        WebElement link1 = driver.findElement(By.xpath("//a[.=\"Add/Remove Elements\"]"));
+
+//        List<WebElement> links = driver.findElements(By.xpath("//a"));
+
+//        WebElement wrongLink = driver.findElement(By.xpath("//hhhhhh"));
+
+//        List<WebElement> wrongLinks = driver.findElements(By.xpath("//fffffff"));
+//        System.out.println(wrongLinks);
+
+
+//        link1.click();
+
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        WebElement textInput = driver.findElement(By.name("email"));
+//        textInput.sendKeys("abc@abc.com" + Keys.ENTER);
+
+        System.out.println(driver.findElement(By.xpath("//label")).getText());
+
+    }
+
+
+}
